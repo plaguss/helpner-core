5e987f2140eb0e574e1f7e5b3332d8b048587391
agustin <agustin.piqueres@gmail.com>
2023-01-24 19:16:08 +0100

    feat: removed checks of wrong training data

--- scripts/convert.py
+++ scripts/convert.py
@@ -5,11 +5,12 @@ https://spacy.io/usage/training#training-data
 https://github.com/explosion/projects/blob/v3/tutorials/ner_fashion_brands/scripts/preprocess.py
 """
 
-import typer
-import srsly
 from pathlib import Path
-from spacy.tokens import DocBin
+
 import spacy
+import srsly
+import typer
+from spacy.tokens import DocBin
 
 
 def main(
@@ -18,26 +19,15 @@ def main(
 ):
     nlp = spacy.blank("en")
     doc_bin = DocBin()
-    i = 0
     for eg in srsly.read_jsonl(input_path):
 
         doc = nlp(eg["message"])
 
-        if doc is None:
-            raise ValueError("??")
-        ents = []
-        for label, start, end in eg["annotations"]:
-            ents.append(doc.char_span(start, end, label=label))
-
-        if len(ents) > 0:
-            try:
-                doc.ents = ents
-            except TypeError:
-                print(f"Message i={i} can't be created, review cli_help_maker generator.")
-                continue
-
+        doc.ents = [
+            doc.char_span(start, end, label=label)
+            for label, start, end in eg["annotations"]
+        ]
         doc_bin.add(doc)
-        i += 1
 
     doc_bin.to_disk(output_path)
     print(f"Processed {len(doc_bin)} documents: {output_path.name}")

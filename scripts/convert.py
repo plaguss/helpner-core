b421f3df3b96ed13a3441bb218dea7c943db7328
agustin <agustin.piqueres@gmail.com>
2023-02-03 17:58:37 +0100

    fix: control errors on messages generated, there is a bug still

--- scripts/convert.py
+++ scripts/convert.py
@@ -19,14 +19,29 @@ def main(
 ):
     nlp = spacy.blank("en")
     doc_bin = DocBin()
-    for eg in srsly.read_jsonl(input_path):
+    for i, eg in enumerate(srsly.read_jsonl(input_path)):
 
         doc = nlp(eg["message"])
 
-        doc.ents = [
-            doc.char_span(start, end, label=label)
-            for label, start, end in eg["annotations"]
-        ]
+        try:
+            doc.ents = [
+                doc.char_span(start, end, label=label)
+                for label, start, end in eg["annotations"]
+            ]
+        except Exception as e:
+            # There is a bug in cli-help-maker?
+            # Some messages failed with the following error:
+            # File "/home/agustin/github_repos/helpner-core/scripts/convert.py", line 39, in main
+            #     raise e from Exception
+            # File "/home/agustin/github_repos/helpner-core/scripts/convert.py", line 27, in main
+            #     doc.ents = [
+            #     ^^^^^^^^
+            # File "spacy/tokens/doc.pyx", line 758, in spacy.tokens.doc.Doc.ents.__set__
+            # File "spacy/tokens/doc.pyx", line 1974, in spacy.tokens.doc.get_entity_info
+            # TypeError: object of type 'NoneType' has no len()
+            print(f"Couldn't process message: {i}")
+            continue
+
         doc_bin.add(doc)
 
     doc_bin.to_disk(output_path)
